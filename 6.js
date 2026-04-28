7991fab9b7564a8406f8c1a1f66b42f7cce5a6e0
ornella leilla <ornellaleilla@gmail.com>
2026-04-28 19:17:28 +0300

    added a function

--- 6.js
+++ 6.js
@@ -1,1 +1,20 @@
+// function drawStars(n){
+//     for(i = 0;i < n; i++){
+//         let star = "*";
+//         console.log(star);
+//     }
+// }
 
+// console.log(drawStars(5));
+
+function drawStars(n){
+     let star = '';
+    for (i=0; i<n; i++){
+       
+        star=star+'*'
+    }
+    console.log(star);
+    
+}
+
+drawStars(4);
