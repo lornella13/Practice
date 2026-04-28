e830246f541824c7aeeccba488e7ba1b8253a47b
ornella leilla <ornellaleilla@gmail.com>
2026-04-28 19:40:41 +0300

    added Q7 func

--- 9.js
+++ 9.js
@@ -1,1 +1,10 @@
 
+function findCheck(arr, target){
+    for (i in arr){
+        if (arr[i] == target){
+            console.log("Found it");
+        }
+    }
+}
+
+findCheck(["banana", "apple", "kiwi"], "kiwi");
